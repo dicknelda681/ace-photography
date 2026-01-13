1b176f66924ce04c9e1066a3ee7bff46461084bb
dicknelda681 <dicknelda681@gmail.com>
2026-01-13 15:54:19 +0530

    Up

--- index.php
+++ index.php
@@ -744,7 +744,19 @@
     
   </section>
   <!-- footer section -->
-
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/69661d29dd7ffd19804e5beb/1jere3rc1';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
   <script type="text/javascript" src="./assets/jquery-3.4.1.min.js.download"></script>
   <script type="text/javascript" src="./assets/bootstrap.js.download"></script>
   <script type="text/javascript" src="./assets/slick.min.js.download"></script>
